29d7ef078f0ad56bca202372ea963d164a8a980e
Torah <95339155+Termng@users.noreply.github.com>
2023-11-15 09:44:33 +0100

    Fixed Broken links

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -2,14 +2,14 @@ class SpecialHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav class="navi">
-                <button class="logo"> <a href="index.html">PRIVACY</a></button>
+                <button class="logo"> <a href="/index.html">PRIVACY</a></button>
      
                 <ul class = 'link-group'>
-                    <li><a href="pricing.html">Pricing & Plans</a></li>
-                    <li><a href="virtualCards.html">What are Virtual Cards ?</a></li>
+                    <li><a href="/pages/pricing.html">Pricing & Plans</a></li>
+                    <li><a href="/pages/virtualCards.html">What are Virtual Cards ?</a></li>
                     <li><a href="">Support</a></li>
-                    <li><a href="aboutUs.html">About Us</a></li>
-                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="/pages/aboutUs.html">About Us</a></li>
+                    <li><a href="/pages/blog.html">Blog</a></li>
                 </ul>
      
                 <div class = "btn-group">
